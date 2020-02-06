c5612938b58e3f34126b4562a93035985bbad2ba
Tautvydas Kubolis <tautvydas.kubolis@hq.unity3d.com>
2020-02-06 09:46:19 +0200

    init

---
+++ Editor/AssetbundleBuilder/AssetbundleCreator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Packages.MEMCoreUnity.Editor.AssetbundleBuilder
+{
+    public class AssetbundleCreator
+    {
+        [MenuItem("MEM/Build Assetbundles")]
+        public static void BuildAssetbundles()
+        {
+            Debug.Log("done");
+        }
+    }
+}
